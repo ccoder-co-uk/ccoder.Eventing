eb2734615c7b99efa216cbcfb1a5682ea243e24f
Paul Ward <paul.ward@corporatelinx.com>
2024-04-25 12:50:47 +0100

    MINOR FIX: add full exception details to event exception logging.

--- src/EventLibrary/Services/Foundation/EventService.cs
+++ src/EventLibrary/Services/Foundation/EventService.cs
@@ -2,37 +2,37 @@
 using EventLibrary.Services.Foundation.Interfaces;
 using Microsoft.Extensions.Logging;
 
-namespace EventLibrary.Services.Foundation
+namespace EventLibrary.Services.Foundation;
+
+public class EventService<T> : IEventService<T>
 {
-    public class EventService<T> : IEventService<T>
+    readonly IEventBroker<T> eventStorageBroker;
+    private readonly ILogger<EventService<T>> log;
+
+    public EventService(IEventBroker<T> eventStorageBroker, ILogger<EventService<T>> log)
     {
-        readonly IEventBroker<T> eventStorageBroker;
-        private readonly ILogger<EventService<T>> log;
+        this.eventStorageBroker = eventStorageBroker;
+        this.log = log;
+    }
 
-        public EventService(IEventBroker<T> eventStorageBroker, ILogger<EventService<T>> log)
+    public void ListenToEvent(string name, Func<IServiceProvider, T, ValueTask> handler) =>
+        eventStorageBroker.ListenToEvent(name, handler);
+
+    public async ValueTask RaiseEventAsync(string name, IServiceProvider serviceProvider, T message)
+    {
+        try
         {
-            this.eventStorageBroker = eventStorageBroker;
-            this.log = log;
+            IEnumerable<Func<IServiceProvider, T, ValueTask>> handlers =
+                eventStorageBroker.GetHandlers(name);
+
+            foreach (var handler in handlers)
+                await handler.Invoke(serviceProvider, message);
         }
-
-        public void ListenToEvent(string name, Func<IServiceProvider, T, ValueTask> handler) =>
-            eventStorageBroker.ListenToEvent(name, handler);
-
-        public async ValueTask RaiseEventAsync(string name, IServiceProvider serviceProvider, T message)
+        catch (Exception ex) 
         {
-            try
-            {
-                IEnumerable<Func<IServiceProvider, T, ValueTask>> handlers =
-                    eventStorageBroker.GetHandlers(name);
-
-                foreach (var handler in handlers)
-                    await handler.Invoke(serviceProvider, message);
-            }
-            catch (Exception ex) 
-            {
-                log.LogError($"Exception thrown whilst raising {name} event", ex);
-                throw;
-            }
+            log.LogError($"Exception thrown whilst raising {name} event", ex);
+            log.LogError($"{ex.Message}\n{ex.StackTrace}", ex);
+            throw;
         }
     }
 }
